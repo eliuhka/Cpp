fa0c7fc90c6a65396afff71fe870481cac00c2a3
erki <56818453+eliuhka@users.noreply.github.com>
2020-09-25 14:38:36 +0300

    iseseisev töö 25,09

---
+++ Iseseisev töö 25.09/IS2YL1/cmake-build-debug/CMakeFiles/IS2YL1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IS2YL1.dir/main.cpp.obj"
+  "IS2YL1.exe"
+  "IS2YL1.exe.manifest"
+  "IS2YL1.pdb"
+  "libIS2YL1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/IS2YL1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Iseseisev töö 25.09/IS2YL1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    int n=5;
+    int a[100];
+    int odd=0;
+    int even=0;
+    cout << "enter your 5 numbers"<<endl;
+
+    for(i=0; i<n; i++){
+        cin>>a[i];
+    }
+    cout<< "\nODD Numbers:"<<endl;
+    for(i=0;i<n;i++){
+        if(a[i]%2==1)
+            cout<<" " <<a[i]<<++odd;
+    }
+    cout<< "\nEVEN Numbers:"<<endl;
+    for(i=0; i<n; i++){
+        if(a[i]%2==0)
+            cout << " " <<a[i]<<++even;
+
+    }
+    cout<<"\nThere was "<<odd<<" odd numbers and "<<even<<" even numbers"<<endl;
+}
